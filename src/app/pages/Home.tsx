29913b09ce33af6b3feec4487389c836e20b860c
rozam007 <rozam0040@gmail.com>
2024-08-09 16:37:31 +0500

    Add Tag line 'Engineer software solution'

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -52,7 +52,7 @@ export default function Home({ setSelectedIndex }: Props) {
               justifyContent={{ xs: "center", sm: "flex-start" }}
             >
               <Typography variant="subtitle1" gutterBottom>
-                Always difficult, always beautiful
+                Engineer Software Solutions
                 {/* Better an{' '}
                 <Box fontWeight="fontWeightMedium" display="inline">
                   oops
