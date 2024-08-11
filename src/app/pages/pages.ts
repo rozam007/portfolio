a8a27c4ddfa438815f720e508cbf0128a70295f1
rozam007 <rozam0040@gmail.com>
2024-08-11 18:37:38 +0500

    comment accomplishments and docs

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -5,6 +5,6 @@ export const pages = [
   { index: 3, name: 'education.md', route: '/education', visible: true },
   { index: 4, name: 'projects.md', route: '/projects', visible: true },
   { index: 5, name: 'certificates.md', route: '/certificates', visible: true },
-  { index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
-  { index: 7, name: 'docs.md', route: '/docs', visible: false },  
+  // { index: 6, name: 'accomplishments.md', route: '/accomplishments', visible: true },  
+  // { index: 7, name: 'docs.md', route: '/docs', visible: false },  
 ];
